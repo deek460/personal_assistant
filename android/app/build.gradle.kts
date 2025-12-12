44161b5f5199677c38ffe666eae16c7bda5bbc8d
Saumya <kri.saumya@gmail.com>
2025-12-12 17:46:58 +0530

    NDK upgrade

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
     namespace = "com.example.personal_assistant"
     compileSdk = 36
     buildToolsVersion = "36.0.0"
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
